b0a6b4b583f939691f51ebd8720c59a90b01fe8d
Nirav Patel <napatel@wpi.edu>
2013-05-26 23:03:46 -0400

    Initial Commit

--- daVinciGazeboPlugins/srv_gen/lisp/_package_SetJointState.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package daVinciGazeboPlugins-srv)
-(cl:export '(JOINT_STATE-VAL
-          JOINT_STATE
-          SUCCESS-VAL
-          SUCCESS
-          STATUS_MESSAGE-VAL
-          STATUS_MESSAGE
-))
