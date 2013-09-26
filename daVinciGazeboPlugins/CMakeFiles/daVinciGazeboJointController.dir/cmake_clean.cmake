f6464409c1def282d943633655d088945783f19d
Adnan <adnan@ad.(none)>
2013-09-26 16:26:46 -0400

    changed name of urdf

---
+++ daVinciGazeboPlugins/CMakeFiles/daVinciGazeboJointController.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "srv_gen"
+  "src/daVinciGazeboPlugins/srv"
+  "srv_gen"
+  "CMakeFiles/daVinciGazeboJointController.dir/src/daVinciGazeboJointController.o"
+  "lib/libdaVinciGazeboJointController.pdb"
+  "lib/libdaVinciGazeboJointController.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/daVinciGazeboJointController.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
