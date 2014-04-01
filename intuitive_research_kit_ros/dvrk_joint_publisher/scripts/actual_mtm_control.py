eb47b610c3a38c2d80a30f038fa38f65689ac402
Adnan Munawar <amunawar@wpi.edu>
2014-04-01 00:01:20 -0400

    Added python script for controlling actual mtm

--- intuitive_research_kit_ros/dvrk_joint_publisher/scripts/actual_mtm_control.py
+++ intuitive_research_kit_ros/dvrk_joint_publisher/scripts/actual_mtm_control.py
@@ -6,26 +6,27 @@ from sensor_msgs.msg import JointState
 jnt_msg = JointState()
 
 def jnt_pos_cb(msg):
-    if len(msg.position) != 12:
-        print "Error msg.position len was supposed to be 12 but is = ", len(msg.position)
+    if len(msg.position) != 8:
+        print "Error msg.position len was supposed to be 8 but is = ", len(msg.position)
         pass
     else:
         jnt_msg.position = []
         jnt_msg.position.append(msg.position[0])
         jnt_msg.position.append(msg.position[1])
+        jnt_msg.position.append(msg.position[2])
+        jnt_msg.position.append(msg.position[3])
+        jnt_msg.position.append(msg.position[4])
+        jnt_msg.position.append(msg.position[5])
         jnt_msg.position.append(msg.position[6])
         jnt_msg.position.append(msg.position[7])
-        jnt_msg.position.append(msg.position[8])
-        jnt_msg.position.append(msg.position[9])
-        jnt_msg.position.append(msg.position[10])
 
 
 def main():
     # initialize ROS node
-    rospy.init_node('psm_joint_publisher')
+    rospy.init_node('mtm_joint_publisher')
 
     # create a psm publisher
-    jnt_pub = rospy.Publisher('/dvrk_psm/set_position_joint', JointState)
+    jnt_pub = rospy.Publisher('/dvrk_mtm/set_position_joint', JointState)
 
     # create psm joint position subscriber
     jnt_pos_sub = rospy.Subscriber(
@@ -35,13 +36,13 @@ def main():
 
 
     # initialize jnt_msg
-    jnt_msg.name = ['one_outer_yaw_joint',
-                    'one_outer_pitch_joint_1',
-                    'one_outer_insertion_joint', 
-                    'one_outer_roll_joint',
-                    'one_outer_wrist_pitch_joint',
-                    'one_outer_wrist_yaw_joint',
-		    														'one_outer_wrist_open_angle_joint_1']
+    jnt_msg.name = ['right_outer_yaw_joint',
+                    'right_shoulder_pitch_joint',
+                    'right_elbow_pitch_joint', 
+                    'right_wrist_platform_joint',
+                    'right_wrist_pitch_joint',
+                    'right_wrist_yaw_joint',
+                    'right_wrist_roll_joint']
 
     # loop until ctrl-c
     rate = rospy.Rate(50);     # 50 hz
