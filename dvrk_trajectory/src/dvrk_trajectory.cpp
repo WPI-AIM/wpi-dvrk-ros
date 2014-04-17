bb0a7fc504c8c98e791be70c5f00a4e9fd3f8873
Adnan Munawar <amunawar@wpi.edu>
2014-04-17 03:03:30 -0400

    Added gripper pinch publisher event and added publishing trajectory via pinch events

--- dvrk_trajectory/src/dvrk_trajectory.cpp
+++ dvrk_trajectory/src/dvrk_trajectory.cpp
@@ -3,21 +3,40 @@
 #include<sensor_msgs/JointState.h>
 #include<vector>
 #include<std_msgs/Bool.h>
+#include<std_msgs/Empty.h>
 #include<iostream>
 
 class Traj
 {
 public:
+    Traj();
     void pose_cb(const geometry_msgs::PoseConstPtr & pose);
     void coag_cb(const std_msgs::BoolConstPtr & state);
     void jointstate_cb(const sensor_msgs::JointStateConstPtr & js);
+    void gripper_pinch_cb(const std_msgs::EmptyConstPtr & empty);
+
+protected:
 
     std::vector<geometry_msgs::Pose> dvrk_pose;
     std::vector<sensor_msgs::JointState> dvrk_js;
     std::vector<geometry_msgs::Pose> dvrk_traj_pose;
     std::vector<sensor_msgs::JointState> dvrk_traj_js;
 
+    ros::NodeHandle node;
+
+    ros::Publisher traj_pub;
+    ros::Subscriber caog_sub;
+    ros::Subscriber pose_sub;
+    ros::Subscriber js_sub;
 };
+
+Traj::Traj()
+{
+    this->caog_sub = node.subscribe("/dvrk_footpedal/coag_state",1000, &Traj::coag_cb, this);
+    this->pose_sub = node.subscribe("/dvrk_mtm/cartesian_pose_current",1000, &Traj::pose_cb,this);
+    this->js_sub = node.subscribe("/dvrk_mtm/joint_position_current",1000,&Traj::jointstate_cb,this);
+    this->traj_pub = node.advertise<geometry_msgs::Pose>("/dvrk_mtm/trajectory_poses",1);
+}
 
 void Traj::pose_cb(const geometry_msgs::PoseConstPtr & pose)
 {
@@ -50,32 +69,26 @@ void Traj::coag_cb(const std_msgs::BoolConstPtr & state)
     }
 }
 
+void Traj::gripper_pinch_cb(const std_msgs::EmptyConstPtr &empty)
+{
+    if (this->dvrk_traj_pose.size()>0)
+        for(int i=0 ; i < dvrk_traj_pose.size() ; i++)
+        {
+            this->traj_pub.publish(dvrk_traj_pose.at(i));
+            ros::spinOnce();
+        }
+}
+
+
+
 
 int main(int argc,char **argv )
 {
     ros::init(argc,argv,"dvrk_trajectory_node");
-    ros::NodeHandle node;
     Traj dvrk;
-    ros::Subscriber coag_sub = node.subscribe("/dvrk_footpedal/coag_state",1000, &Traj::coag_cb, &dvrk);
-    ros::Subscriber pose_sub = node.subscribe("/dvrk_mtm/cartesian_pose_current",1000, &Traj::pose_cb, &dvrk);
-    ros::Subscriber js_sub = node.subscribe("/dvrk_mtm/joint_position_current",1000,&Traj::jointstate_cb,&dvrk);
-    ros::Publisher  pose_pub = node.advertise<geometry_msgs::Pose>("/dvrk_trajectory/Pose",1);
-    ros::Publisher js_pub = node.advertise<sensor_msgs::JointState>("dvrk_trajectory/JointState",1);
-    ros::Rate rate(200);
     while(ros::ok())
     {
-        if (dvrk.dvrk_traj_pose.size()>0)
-            for(int i=0 ; i < dvrk.dvrk_traj_pose.size() ; i++)
-            {
-                pose_pub.publish(dvrk.dvrk_traj_pose.at(i));
-            }
-        if (dvrk.dvrk_traj_js.size()>0)
-            for(int i=0 ; i < dvrk.dvrk_traj_js.size() ; i++)
-            {
-                js_pub.publish(dvrk.dvrk_traj_js.at(i));
-            }
         ros::spinOnce();
-        rate.sleep();
     }
 
     return 0;
