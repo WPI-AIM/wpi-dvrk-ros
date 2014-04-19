6a836faa88ddeb4e5f9ac63e04b90438ec62dbbf
Adnan Munawar <amunawar@wpi.edu>
2014-04-19 15:31:54 -0400

    Minor changes for publishing the size of trajectory in terms of poses

--- psm_moveit_ik/src/psmMP.cpp
+++ psm_moveit_ik/src/psmMP.cpp
@@ -13,6 +13,7 @@
 #include <moveit/robot_state/robot_state.h>
 #include <moveit_msgs/PlanningScene.h>
 #include <ros/time.h>
+#include <std_msgs/UInt64.h>
 
 class Kinematic_group{
 
@@ -20,10 +21,12 @@ public:
     Kinematic_group();
     moveit::planning_interface::MoveGroup * group;
     ros::Subscriber trajectory_sub;
+    ros::Subscriber trajectory_size_sub;
     ros::Publisher trajectory_pub;
-    bool path_computed;
+    bool traj_generate;
 
     void trajectory_cb(const geometry_msgs::PoseConstPtr & pose);
+    void trajectory_size_cb(const std_msgs::UInt64ConstPtr & size);
     void generate_trajectory();
     void publish_trajectory();
 
@@ -32,31 +35,41 @@ protected:
     std::vector<geometry_msgs::Pose> waypoint;
     moveit_msgs::RobotTrajectory path;
     ros::Rate *rate;
+    std_msgs::UInt64 traj_size;
 };
 
 Kinematic_group::Kinematic_group()
 {
     this->rate = new ros::Rate(500);
-    this->path_computed = false;
+    this->traj_generate = false;
     this->group = new moveit::planning_interface::MoveGroup("full_chain");
-    this->trajectory_sub = node.subscribe("/mtm/trajectory_poses",1000,&Kinematic_group::trajectory_cb,this);
-    this->trajectory_pub = node.advertise<moveit_msgs::RobotTrajectory>("/moveit_mtm/waypoint",1);
+    this->trajectory_sub = node.subscribe(
+                "/mtm/trajectory_poses",1000,&Kinematic_group::trajectory_cb,this);
+    this->trajectory_size_sub = node.subscribe(
+                "/mtm/trajectory_poses_size",1000,&Kinematic_group::trajectory_size_cb,this);
+    this->trajectory_pub = node.advertise<moveit_msgs::RobotTrajectory>(
+                "/moveit_mtm/waypoint",1);
+}
+
+void Kinematic_group::trajectory_size_cb(const std_msgs::UInt64ConstPtr &size)
+{
+    this->traj_size.data = size->data;
 }
 
 void Kinematic_group::trajectory_cb(const geometry_msgs::PoseConstPtr &pose)
 {
     this->waypoint.push_back(*pose.get());
+    if (this->waypoint.size() == this->traj_size.data)
+    {
+        this->traj_generate = true;
+    }
 }
 
 void Kinematic_group::generate_trajectory()
 {
     double fraction = this->group->computeCartesianPath(
                 this->waypoint,0.1,0,this->path);
-    this->path_computed = true;
-}
 
-void Kinematic_group::publish_trajectory()
-{
     if(this->path.joint_trajectory.points.size() > 0)
     {
     this->trajectory_pub.publish(this->path);
@@ -76,9 +89,10 @@ int main(int argc, char ** argv)
     Kinematic_group traj;
     while(ros::ok())
     {
-        if(traj.path_computed == true)
+        if(traj.traj_generate == true)
         {
-            traj.publish_trajectory();
+            traj.generate_trajectory();
+            traj.traj_generate = false;
         }
         ros::spinOnce();
     }
