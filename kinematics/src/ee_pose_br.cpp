7b3add26eba63d1a6c915bfe09e9b4c3fb541fb3
Adnan Munawar <amunawar@wpi.edu>
2014-04-22 19:33:47 -0400

    ee_pose_br.cpp in fully functional working state now

--- kinematics/src/ee_pose_br.cpp
+++ kinematics/src/ee_pose_br.cpp
@@ -1,28 +1,53 @@
+// Author: Adnan Munawar
+// Email : amunawar@wpi.edu
+
 #include<ros/ros.h>
 #include<tf/tf.h>
 #include<tf/transform_broadcaster.h>
 #include<tf/transform_listener.h>
 #include<geometry_msgs/PoseStamped.h>
 
-class cartesian_pose{
+
+// The purpose of this class is to implement the same cartesian pose as reported by the real MTMs. For this purpose, two extra frames have
+// been placed. One at origin and one at the end effector, both of which are represented in the daVinci documentation.
+class pose_correction{
 public:
-    cartesian_pose();
+    pose_correction();
     void listen();
+    void broadcast();
     tf::TransformListener listener;
+    tf::TransformBroadcaster brodcaster;
     tf::StampedTransform tran;
+    tf::StampedTransform base_tr;
+    tf::StampedTransform ee_tr;
     ros::Publisher pub;
     ros::NodeHandle node;
 };
 
-cartesian_pose::cartesian_pose()
+pose_correction::pose_correction()
 {
+    this->base_tr.setOrigin(tf::Vector3(0.0,0.0,-0.190));
+    this->base_tr.setRotation(tf::createQuaternionFromRPY(0.000, 0.000, 0.000));
+    this->ee_tr.setOrigin(tf::Vector3(0.000, 0.000, 0.039));
+    this->ee_tr.setRotation(tf::createQuaternionFromRPY(0.0,3.142,1.571));
     this->pub = this->node.advertise<geometry_msgs::PoseStamped>("/simulated_mtm/end_effector_pose",1);
+
+    //Broadcast the frames in the constructor.
+    this->broadcast();
 }
 
-void cartesian_pose::listen()
+void pose_correction::broadcast()
+{
+    this->brodcaster.sendTransform(tf::StampedTransform(
+                                       this->base_tr,ros::Time::now(),"right_top_panel","base_frame"));
+    this->brodcaster.sendTransform(tf::StampedTransform(
+                                       this->ee_tr,ros::Time::now(),"right_wrist_roll_link","ee_frame"));
+}
+
+void pose_correction::listen()
 {
     try{
-        this->listener.lookupTransform("right_back_parallel_link","right_wrist_roll_link",ros::Time(),this->tran);
+        this->listener.lookupTransform("base_frame","ee_frame",ros::Time(),this->tran);
     }
     catch(tf::TransformException ex)
     {
@@ -35,11 +60,12 @@ int main(int argc, char ** argv)
     ros::init(argc,argv,"end_effector_pose_node");
     ros::NodeHandle node;
     ros::Rate rate(500);
-    cartesian_pose cart_pos;
-    cart_pos.listener.waitForTransform("right_back_parallel_link","right_wrist_roll_link",ros::Time(),ros::Duration(1.0));
+    pose_correction cart_pos;
+    cart_pos.listener.waitForTransform("base_frame","ee_frame",ros::Time(),ros::Duration(1.0));
     geometry_msgs::PoseStamped pose_stamped;
     while(ros::ok())
     {
+        cart_pos.broadcast();
         cart_pos.listen();
         pose_stamped.header.stamp = cart_pos.tran.stamp_;
         pose_stamped.pose.position.x = cart_pos.tran.getOrigin().getX();
