eec36f4311e6b87230e9f591998c152ca677da5b
Adnan <adnan.munawar@live.com>
2016-02-20 21:58:23 -0500

    First working code, a wall in y direction

--- mtm_haptics/src/mtm_haptics.cpp
+++ mtm_haptics/src/mtm_haptics.cpp
@@ -3,6 +3,8 @@
 #include<ros/ros.h>
 #include<sensor_msgs/JointState.h>
 #include<geometry_msgs/Pose.h>
+#include<geometry_msgs/Vector3.h>
+#include<geometry_msgs/Wrench.h>
 #include<std_msgs/String.h>
 
 
@@ -13,11 +15,15 @@ public:
 
     ros::Publisher crt_torque_pub;
     ros::Publisher jnt_torque_pub;
+    ros::Publisher robot_state_pub;
 
     ros::Subscriber jnt_pos_sub;
     ros::Subscriber jnt_torque_sub;
     ros::Subscriber crt_pos_sub;
     ros::Subscriber cur_robot_state_sub;
+    void haptic_feeback_plane(geometry_msgs::Point set_point, bool _collision);
+    void set_effort_mode();
+    ros::Rate *rate_;
 
 
 
@@ -33,13 +39,33 @@ protected:
     std::vector<double> cur_mtm_effort;
     std::vector<double> cur_mtm_jnt_pos;
     std::string cur_robot_state;
+    std_msgs::String robot_state_cmd;
+    geometry_msgs::Wrench haptic_feedback;
+    geometry_msgs::Vector3 Kp;
+    geometry_msgs::Vector3 dX;
+
+
 };
 
 MTMHaptics::MTMHaptics(){
 
     jnt_pos_sub = node_.subscribe("/dvrk_mtm/joint_position_current", 10, &MTMHaptics::jnt_pos_cb, this);
-    jnt_torque_sub = node_.subscribe("/dvrk_mtm/joint_effort_current", 10, &MTMHaptics::jnt_torque_cb, this);
+//    jnt_torque_sub = node_.subscribe("/dvrk_mtm/joint_effort_current", 10, &MTMHaptics::jnt_torque_cb, this);
     crt_pos_sub = node_.subscribe("/dvrk_mtm/cartesian_pose_current", 10, &MTMHaptics::crt_pos_cb, this);
+
+    crt_torque_pub = node_.advertise<geometry_msgs::Wrench>("/dvrk_mtm/set_wrench_body",10);
+    jnt_torque_pub = node_.advertise<sensor_msgs::JointState>("/dvrk_mtm/set_effort_joint",10);
+    robot_state_pub = node_.advertise<std_msgs::String>("/dvrk_mtm/set_robot_state",10);
+
+    rate_ = new ros::Rate(100);
+
+    Kp.x = 50;
+    Kp.y = 50;
+    Kp.z = 50;
+
+    dX.x = 0.025;
+    dX.y = 0.025;
+    dX.z = 0.025;
 
 }
 
@@ -61,8 +87,55 @@ void MTMHaptics::cur_robot_state_cb(const std_msgs::StringConstPtr &msg){
     cur_robot_state = msg->data;
 }
 
-int main(int argc, char ** argv){
+void MTMHaptics::set_effort_mode(){
+    robot_state_cmd.data = "DVRK_EFFORT_CARTESIAN";
+    robot_state_pub.publish(robot_state_cmd);
+    ROS_INFO("Setting Robot state as: %s",robot_state_cmd.data.c_str());
+    rate_->sleep();
+}
 
-printf("Hello ROS");
-return 0;
+void MTMHaptics::haptic_feeback_plane(geometry_msgs::Point set_point, bool _collision){
+    if(strcmp(this->cur_robot_state.c_str(),"DVRK_EFFORT_CARTESIAN")){
+        geometry_msgs::Point error;
+        error.x = set_point.x - cur_mtm_pose.position.x;
+        error.y = set_point.y - cur_mtm_pose.position.y;
+        error.z = set_point.z - cur_mtm_pose.position.z;
+
+        if (error.y >= 0 && error.y <= dX.y){
+            haptic_feedback.force.x = 0;
+            haptic_feedback.force.y = 0; //Currently applying a force in the Z direction, as Z for tip point towards Y of base, and wrench is being applied in tip frame.
+            haptic_feedback.force.z = Kp.z * error.z;
+            ROS_INFO("Publishing Force");
+            crt_torque_pub.publish(haptic_feedback);
+        }
+        else{
+            haptic_feedback.force.x = 0;
+            haptic_feedback.force.y = 0;
+            haptic_feedback.force.z = 0;
+            crt_torque_pub.publish(haptic_feedback);
+        }
+
+    }
+    else{
+        ROS_WARN("MTM is %s state, it needs to be in DVRK_EFFORT_CARTESIAN STATE",this->cur_robot_state.c_str());
+    }
+}
+
+
+
+int main(int argc, char ** argv){
+    ros::init(argc,argv,"mtm_haptics_node");
+    ros::AsyncSpinner spinner(1);
+    MTMHaptics haptics;
+    spinner.start();
+    sleep(2.0);
+    geometry_msgs::Point haptic_point;
+    haptic_point.y = -0.38;
+
+    haptics.set_effort_mode();
+
+    while (ros::ok()){
+    haptics.haptic_feeback_plane(haptic_point,true);
+    haptics.rate_->sleep();
+    }
 }
