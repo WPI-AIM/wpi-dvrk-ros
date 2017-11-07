9ef1a58b6f5a0a285669ac2e6d33240b6822499a
Adnan <adnan.munawar@live.com>
2017-11-07 13:13:48 -0500

    Cleaning up

--- geomagic_teleop/src/geomagic_psm_teleop.cpp
+++ geomagic_teleop/src/geomagic_psm_teleop.cpp
@@ -4,7 +4,7 @@
 #include "geomagic_teleop/geomagic_psm_teleop.h"
 
 Geomagic_Teleop::Geomagic_Teleop(boost::shared_ptr<ros::NodeHandle> node):
-    _node(node)
+    _node(node), _clutch(false), _coag(false), _first_trigger(false)
 {
     _node->param(std::string("device_name"), _device_name, std::string("Geomagic"));
     _node->param(std::string("arm"), _slave_name, std::string("PSM1"));
@@ -14,7 +14,6 @@ Geomagic_Teleop::Geomagic_Teleop(boost::shared_ptr<ros::NodeHandle> node):
     arm_psm.reset(new DVRK_Arm(_slave_name));
     cur_gFrame.reset(new Frame);
     pre_gFrame.reset(new Frame);
-    rotPsm2Geo.setRPY(-M_PI, 0, M_PI/2);
 
     geomagic_joy_sub = _node->subscribe("/" + _device_name + "/joy",10,&Geomagic_Teleop::geomagic_joy_cb, this);
     geomagic_pose_sub = _node->subscribe("/" + _device_name + "/pose",10,&Geomagic_Teleop::geomagic_pose_cb, this);
@@ -25,7 +24,6 @@ Geomagic_Teleop::Geomagic_Teleop(boost::shared_ptr<ros::NodeHandle> node):
     geomagic_joy_pre.axes.resize(6);
     geomagic_joy_cur.buttons.resize(2);
     omni_feedback.lock.resize(3);
-    _clutch = false; _coag = false; _first_trigger = false;
     ros::spinOnce();
     arm_psm->set_mode(arm_psm->_m_cart_pos_mode);
     sleep(1);
@@ -38,21 +36,6 @@ void Geomagic_Teleop::align_end_effectors(){
     tf::Quaternion rot_psm;
     arm_psm->get_cur_orientation(rot_psm);
     rotPsm2Geo = cur_gFrame->rot_quat.inverse() * rot_psm;
-//    tf::Transform psm_trans;
-//    tf::Quaternion psm_rot, geo_rot, affix_psm_rot;
-//    arm_psm->get_cur_transform(psm_trans);
-//    arm_psm->set_origin_frame(psm_trans);
-//    arm_psm->get_cur_orientation(psm_rot);
-//    tf::quaternionMsgToTF(geomagic_pose_cur.orientation, geo_rot);
-//    affix_psm_rot = psm_rot.inverse() * geo_rot;
-//    if(std::abs(affix_psm_rot.length() - 1) < 1e-6 ){
-//        arm_psm->affix_tip_frame_rot(affix_psm_rot);
-////        arm_psm->get_cur_transform(psm_trans);
-////        arm_psm->set_transform(psm_trans);
-//    }
-//    else{
-//        ROS_ERROR("GEO ROT NON VALID TO AFFIX TO PSM FRAME %f", affix_psm_rot.length());
-//    }
 }
 
 void Geomagic_Teleop::PosetoFrame(const geometry_msgs::Pose &pose, FramePtr frame){
@@ -64,7 +47,7 @@ void Geomagic_Teleop::PosetoFrame(const geometry_msgs::Pose &pose, FramePtr fram
 }
 
 void Geomagic_Teleop::geomagic_pose_cb(const geometry_msgs::PoseStampedConstPtr msg){
-    PosetoFrame(geomagic_pose_cur, pre_gFrame);
+    pre_gFrame = cur_gFrame;
     geomagic_pose_pre = geomagic_pose_cur;
     geomagic_pose_cur = msg->pose;
     PosetoFrame(geomagic_pose_cur, cur_gFrame);
@@ -103,34 +86,24 @@ void Geomagic_Teleop::run(){
     psmTrans.setOrigin(psmTrans.getOrigin() + scale*(cur_gFrame->trans.getOrigin() - pre_gFrame->trans.getOrigin()));
     psmTrans.setRotation(cur_gFrame->trans.getRotation() * rotPsm2Geo);
 
-    if(_clutch || _coag){
-        if(_first_trigger == false){
-            _first_trigger = true;
-            for(u_int i = 0 ; i < 3 ; i++){
-                omni_feedback.lock[i] = false;
-            }
-        }
-        else{
-            omni_feedback.position.x = geomagic_joy_cur.axes[0];
-            omni_feedback.position.y = geomagic_joy_cur.axes[1];
-            omni_feedback.position.z = geomagic_joy_cur.axes[2];
+    if(_clutch || _coag){     
+        omni_feedback.position.x = geomagic_pose_cur.position.x;
+        omni_feedback.position.y = geomagic_pose_cur.position.y;
+        omni_feedback.position.z = geomagic_pose_cur.position.z;
 
-            omni_feedback.force.x = 0;
-            omni_feedback.force.y = 0;
-            omni_feedback.force.z = 0;
+        omni_feedback.force.x = 0; omni_feedback.force.y = 0; omni_feedback.force.z = 0;
 
-            for(u_int i = 0 ; i < 3 ; i++){
-                omni_feedback.lock[i] = true;
-            }
+        for(u_int i = 0 ; i < 3 ; i++){
+            omni_feedback.lock[i] = true;
         }
         if(_coag){
             arm_psm->set_transform(psmTrans);
         }
     }
     else{
-        _first_trigger = false;
         align_end_effectors();
     }
+
     geomagic_force_pub.publish(omni_feedback);
     ros::spinOnce();
     rate->sleep();
